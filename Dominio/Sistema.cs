22f3f2667d855c956661b09d33a19bdffd39bc40
Lautaro Zapata <oratualzc@gmail.com>
2025-04-03 20:52:01 -0300

    obligatorio

---
+++ Dominio/Sistema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Sistema
+    {
+        private static Sistema instancia;
+        public static Sistema Instancia
+        {
+            get
+            {
+                if(instancia == null) instancia = new Sistema();
+                return instancia;
+            }
+        }
+
+        private Sistema()
+        {
+
+
+        }
+    }
+}
---
+++ Obligatorio1 P2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dominio;
+namespace Obligatorio1_P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sistema unS = Sistema.Instancia;
+        }
+    }
+}
